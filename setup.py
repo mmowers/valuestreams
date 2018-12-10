698929f384b273a878729e9dd27d55235323c0d6
Matt Mowers <Matthew.Mowers@nrel.gov>
2018-12-10 14:34:30 -0500

    Change description of valuestreams package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='valuestreams',
       version='0.1',
-      description='Create pandas dataframe of value streams from cplex MPS file and GAMS gdx solution file of variable levels and marginals',
+      description='Create pandas dataframe of value streams from cplex MPS file and GAMS gdx file of variable levels and marginals from solution',
       url='http://github.com/mmowers/valuestreams',
       author='Matt Mowers',
       license='MIT',
